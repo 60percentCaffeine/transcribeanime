790790dfff31af171d9e6d8ba59533d01b47583b
helpahelpa <helpahelpa>
2026-03-13 00:33:53 +0300

    aaa

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 echo "####################"
 echo "White box assessment -- you are allowed to read logs and srt files used in this" 
 echo "####################"
-poetry run python3 fixtranscribeanime.py -r reference1.srt -o output1.srt transcription1.srt --delay 0 > log.txt
-poetry run python3 compare_srt.py output1.srt cc1.srt
+poetry run python3 fixtranscribeanime.py -r slimetest/sreference04.srt -o soutput04.srt slimetest/stranscription04.srt --delay 0 > log.txt
+poetry run python3 compare_srt.py soutput04.srt slimetest/scc04.srt
 
 echo
 
 echo "####################"
 echo "White box original transcription" 
 echo "####################"
-poetry run python3 compare_srt.py transcription1.srt cc1.srt
+poetry run python3 compare_srt.py slimetest/stranscription04.srt slimetest/scc04.srt
 
 echo
 echo -------------------
@@ -18,12 +18,12 @@ echo
 echo "####################"
 echo "Black box assessment -- you ARE NOT ALLOWED under ANY CIRCUMSTANCES to read logs and srt files used in this, only use the similarity score"
 echo "####################"
-poetry run python3 fixtranscribeanime.py -r ../transcribeanimetest/reference2.srt -o ../transcribeanimetest/output2.srt ../transcribeanimetest/transcription2.srt --delay 0 > /dev/null
-poetry run python3 compare_srt.py ../transcribeanimetest/output2.srt ../transcribeanimetest/cc2.srt
+poetry run python3 fixtranscribeanime.py -r ../slimetest/sreference06.srt -o ../slimetest/soutput06.srt ../slimetest/stranscription06.srt --delay 0 > /dev/null
+poetry run python3 compare_srt.py ../slimetest/soutput06.srt ../slimetest/scc06.srt
 
 echo
 
 echo "####################"
 echo "Black box original transcription" 
 echo "####################"
-poetry run python3 compare_srt.py ../transcribeanimetest/transcription2.srt ../transcribeanimetest/cc2.srt
+poetry run python3 compare_srt.py ../slimetest/stranscription06.srt ../slimetest/scc06.srt
